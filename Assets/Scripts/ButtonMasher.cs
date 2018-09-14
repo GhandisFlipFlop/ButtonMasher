916e44d4cbbd547ce68d4b774fade020f50ab3a4
GhandisFlipFlop <43068438+GhandisFlipFlop@users.noreply.github.com>
2018-09-14 13:57:27 +0100

    added music and buttonclick

--- Assets/Scripts/ButtonMasher.cs
+++ Assets/Scripts/ButtonMasher.cs
@@ -4,14 +4,31 @@ using UnityEngine;
 
 public class ButtonMasher : MonoBehaviour {
 
+    // Data for our class
+    public AudioSource clickSound; // Sound that will play when we click on the button 
+
 	// Use this for initialization
-	void Start () {
-        Debug.Log("Start method called");
- 
-	}
-	
-	// Update is called once per frame
-	void Update () {
+	    void Start () {
+
+        Debug.Log("Start method was called");
+
+    } // End of Start()
+
+    // Update is called once per frame
+        void Update () {
+
         Debug.Log("Update method was called ");
-	}
-}
+
+    } // End of Update()
+
+
+    // Responds to event from Unity that the object has been clicked
+    private void OnMouseDown()
+    {
+        Debug.Log("onMouseDown method was called");
+        // Trigger our click sound causing buttonClick audio to play
+        clickSound.Play();
+
+    } // End of onMouseDown()
+
+} // End of ButtonMasher class
